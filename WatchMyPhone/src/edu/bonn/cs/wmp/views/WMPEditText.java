dc19f40b1697c32dc68102ffee1442b6561bbff2
ubuntudroid <ubuntudroid@googlemail.com>
2011-05-21 16:55:46 +0200

    WMPEditText.java writes content into existing node
    Added note edit_text into edit_text_test.xml on server. WMPEditText will write it's content into this node. Additionally it will stop writing it's initial content into the node on entering the session as we shall make no changes to the XML as long as participants don't have connected. Therefore the WMPEditText in the prototype is stripped of all initial content, too.

--- WatchMyPhone/src/edu/bonn/cs/wmp/views/WMPEditText.java
+++ WatchMyPhone/src/edu/bonn/cs/wmp/views/WMPEditText.java
@@ -1,6 +1,7 @@
 package edu.bonn.cs.wmp.views;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.Text;
 
 import de.hdm.cefx.concurrency.operations.NodePosition;
@@ -16,10 +17,10 @@ import android.widget.EditText;
 public class WMPEditText extends EditText {
 	
 	private CollabEditingService collabService;
-	private Element el;
 	
 	public WMPEditText(Context context) {
 		super(context);
+		
 		// TODO Auto-generated constructor stub
 	}
 
@@ -42,7 +43,14 @@ public class WMPEditText extends EditText {
 	@Override
 	protected void onTextChanged(CharSequence text, int start, int before,
 			int after) {
+		
 		if (collabService != null && collabService.isReadyForEditing()){
+			// TODO: search element by name (edit_text)
+			Element el = (Element) collabService.getDOMAdapter().getDocument().getElementsByTagName("edit_text").item(0);
+			if (!el.hasChildNodes()){
+				Text content = collabService.createTextNode("");
+				el.appendChild(content);
+			}
 			if (after > 0){
 				// INSERT
 				collabService.insertText(el, null, NodePosition.INSERT_BEFORE, text.subSequence(start, start+after).toString(), start);
@@ -60,11 +68,11 @@ public class WMPEditText extends EditText {
 	public void setCollabEditingService(CollabEditingService collabService){
 		this.collabService = collabService;
 		// TODO: atm node name is hardcoded, obtain ID from WMPComponentRegistry
-		el = collabService.createElement("edit_text");
-		Text content = collabService.createTextNode(this.getText().toString());
-		el.appendChild(content);
-		collabService.insertNode("100", el, null, NodePosition.INSERT_BEFORE);
-		Log.i("WMP", "document state: " + collabService.getDocumentString());
+		// we won't run the following code for now, as propagation should start not sooner than all clients have joined the session
+//		Text content = collabService.createTextNode(this.getText().toString());
+//		el.appendChild(content);
+//		collabService.insertNode("100", el, null, NodePosition.INSERT_BEFORE);
+//		Log.i("WMP", "document state: " + collabService.getDocumentString());
 	}
 
 }
