2be07395b0a8f2aa5e391127471c0c58db02a4b5
ubuntudroid <ubuntudroid@googlemail.com>
2011-06-13 15:02:05 +0200

    Added *.bak file check to wmp_local_server_config.sh
    WARNING: local_server_config.sh and wmp_server_config.sh were renamed to wmp_local_server_config.sh and wmp_ec2_server_config.sh because both were made accessable from everywhere in the filesystem which raised the need for a more specific naming convention.
    
    Previously wmp_local_server_config.sh removed the XML setting files also if there were no bak files to replace them. We now check for the existence of the bak files before deleting the XMLs.

--- MobilisServer/wmp_server_config/wmp_ec2_server_config.sh
+++ MobilisServer/wmp_server_config/wmp_ec2_server_config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#usr/bin/sh
 # This script creates backups of hibernate.xml and MobilisSettings.xml in src/META_INF and replaces them with the ec2 XMPP server configuration as saved in the location folder of these two scripts. It is normally run *before* creating a running jar for deployment on the Amazon EC2 server. Make sure you run local_server_config.sh *after* you've created the jar file.
 # @sven 06.2011
 
