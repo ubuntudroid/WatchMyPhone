5ea642413545c5b98835f86b01da60e446cb8f57
ubuntudroid <ubuntudroid@googlemail.com>
2011-06-14 19:42:29 +0200

    Updated server configs and scripts for remote XMPP server / MySQL database

--- MobilisServer/wmp_server_config/wmp_ec2_server_config.sh
+++ MobilisServer/wmp_server_config/wmp_ec2_server_config.sh
@@ -1,5 +1,6 @@
 #usr/bin/sh
-# This script creates backups of hibernate.xml and MobilisSettings.xml in src/META_INF and replaces them with the ec2 XMPP server configuration as saved in the location folder of these two scripts. It is normally run *before* creating a running jar for deployment on the Amazon EC2 server. Make sure you run local_server_config.sh *after* you've created the jar file.
+# This script creates backups of hibernate.xml and MobilisSettings.xml in src/META_INF and replaces them with the ec2 XMPP server configuration as saved in the location folder of these two scripts. It is normally used to create the config files to connect to our Amazon EC2 XMPP server/MySQL database. Make sure you run local_server_config.sh if you want to use the local server instead.
+# For deploying of MobilisServer on the Amazon EC2 server use the local server configuration.
 # @sven 06.2011
 
 mv ../src/META-INF/hibernate.xml ../src/META-INF/hibernate.xml.bak
--- MobilisServer/wmp_server_config/wmp_local_server_config.sh
+++ MobilisServer/wmp_server_config/wmp_local_server_config.sh
@@ -1,5 +1,5 @@
 #usr/bin/sh
-# This scripts overrides the current Mobilis Server configuration as defined in /src/META-INF/MobilisSettings.xml and hibernate.xml by the default local configuration saved in the appropriate bak files by the a accompanied wmp_server_config.sh script. This script is normally run *after* the wmp_server_config.sh script and after a runnable jar file has been created for deployment on the Amazon EC2 server. It returns the Server project back to normal development state.
+# This scripts overrides the current Mobilis Server configuration as defined in /src/META-INF/MobilisSettings.xml and hibernate.xml by the default local configuration saved in the appropriate bak files by the a accompanied wmp_server_config.sh script. This script is normally run *after* the wmp_server_config.sh script and reverts everything back to a local setup, i.e. a local XMPP server/database. It returns the Server project back to normal development state.
 # To make sure, that the current XMLs are not destroyed without any backups in place, we check for the existence of the bak files *before* deleting the files.
 # @sven 06.2011
 if [ ! -f ../src/META-INF/hibernate.xml.bak ]; then
